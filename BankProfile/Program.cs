25d48453e8c98fadc5b38dfbf58387be2290639b
Tyler Rubin <rubin013@cougars.csusm.edu>
2020-05-12 10:21:01 -0700

    Created Solution
    Created solution to begin project.

---
+++ BankProfile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
