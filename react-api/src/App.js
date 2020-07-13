30d90eee72f7021928264c3760d0551dcf712266
Tyler <tyler.rubin67@yahoo.com>
2020-07-13 13:18:48 -0700

    Added the ability to filter state properties and display desired keys on webpage.

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -4,40 +4,36 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import firebaseconfig from './firebaseconfig';
 import './App.css';
-//import {BrowserRouter as Router,Route,Redirect,Switch, Link} from 'react-router-dom';
+import { Alert, Breadcrumb, BreadcrumbItem, Container, Row, Col, Button } from "shards-react";
 import Contacts from './components/contacts';
 
 const firebaseApp = firebase.initializeApp(firebaseconfig);
 
 //Console testing for API data
-const element = fetch("https://localhost:44358/api/UserInformations")
+const element = fetch("https://localhost:44358/api/UserInformations/45505")
       .then((response) => response.json())
-      .then((data) => console.log('Here is my data returned:', data[0].accountNumber));
+      .then((data) => console.log('Here is my data returned:', data.accountNumber));
 //Console testing
 
 class App extends Component{
-  
-  getVal(){
-    var dataa = [];
-    var conv;
-    const element = fetch("https://localhost:44358/api/UserInformations/45505")
-      .then((response) => response.json())
-      .then(response => dataa.push(response))
-      .then(console.log('the result is', dataa))
-      conv = JSON.stringify(dataa)
-      return conv;
-  }
-
 
   state = {
-    contacts: []
+    contacts: [],
+    datas: []
   }
 
   componentDidMount() {
-    fetch('https://localhost:44358/api/UserInformations')
+    fetch('https://localhost:44358/api/UserInformations/45505')
     .then(res => res.json())
     .then((data) =>  {
       this.setState({ contacts: data })
+    })
+    .catch(console.log)
+
+    fetch('https://localhost:44358/api/UserInformations/45505')
+    .then(res => res.json())
+    .then((data) =>  {
+      this.setState({ datas: data })
     })
     .catch(console.log)
   }
@@ -51,10 +47,31 @@ class App extends Component{
     return (  
       <div className="App">
         <header className="App-header">
-          <img src="https://media.giphy.com/media/9P3DSO2FzzvWxDtdWP/giphy.gif"/>
+         
           {
             user
-              ? <p>Hello, |{this.getVal()}|{user.displayName}<Contacts contacts={this.state.contacts} />
+              ? <p align="left">
+              <Breadcrumb>
+                <BreadcrumbItem>
+                <a href="#">Home  |    </a>
+                </BreadcrumbItem> 
+                <Button theme="danger" onClick={signOut}>Logout Here</Button>
+              </Breadcrumb>
+              <img src="https://media.giphy.com/media/9P3DSO2FzzvWxDtdWP/giphy.gif"/>
+              <p align = "center">Hello, {user.displayName}</p>
+
+              <Container className="dr-example-container">
+              <Row>
+                <Col></Col>
+                <Col sm="25" lg="25">
+                <font face = "Verdana" size = "5">
+                    Your current Account Balance is: <Contacts contacts={this.state.contacts}/> {" "}
+                    </font>
+                </Col>
+                <Col></Col>
+              </Row>
+              </Container>
+              
               </p>
               : <p>Please sign in.</p>   
               
--- react-api/src/components/contacts.js
+++ react-api/src/components/contacts.js
@@ -1,22 +1,29 @@
     // src/components/contacts.js
     import React from 'react'
 
-    const Contacts = ({ contacts }) => {
+    const Contacts = ({ contacts}) => {
       const element = fetch("https://localhost:44358/api/UserInformations/45505")
       .then((response) => response.json())
       .then((data) => console.log('Here is my data returned:', data.accountNumber));
 
       return (
         <div>
-          {contacts.map((contact) => (
+         {Object.keys(contacts).filter(key => key === 'accountBalance').map((item,i) => (
             <div>
-                <h6 class="card-title">{contact.accountNumber}</h6>
-                <h6 class="card-title">{contact.firstName}</h6>
-                <h6 class="card-title">{contact.accountBalance}</h6>
+                <h6 key={i} class="card-title">{contacts[item]}</h6>
             </div>
           ))}
+          
         </div>
       )
     };
 
     export default Contacts;
+
+    /*
+  {Object.keys(datas).filter(key => key === 'age').map((item,i) => (
+            <div>
+                <h6 key={i} class="card-title">{datas[item]}</h6>
+            </div>
+          ))}
+    */
